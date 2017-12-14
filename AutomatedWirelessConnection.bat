51efba67e05b0c05c784341261d216ca1c89c20f
Anthony Vu <anthony.vu2142@gmail.com>
2017-12-14 13:49:57 -0800

    successfully detected if laptop based on hostname

--- AutomatedWirelessConnection.bat
+++ AutomatedWirelessConnection.bat
@@ -1,11 +1,27 @@
+@ECHO OFF
 REM Anthony Vu 12/13/17
 REM This script will connect a computer to the
 REM "guest" network if the device is a laptop
 setlocal
 REM 6DSASTS111222LS
 REM should pull L
-set device=%computername:~13,1%
-
-if %device% == "L" (
-   netsh wlan connect ssid="Guest" name="Guest" interface="Wi-Fi"
+echo %computername%
+set device=%computername:~-2%
+rem echo ON
+set device2=%device:~0,1%
+if "%device2%"=="L" (
+    echo Is Laptop
+	REM add WiFi
+	
+	
+	
+	
+	
+	
+) else (
+	echo Not Laptop
 )
+ECHO OFF
+REM if %device% == "L" (
+REM   netsh wlan connect ssid="Guest" name="Guest" interface="Wi-Fi"
+REM )
